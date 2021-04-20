7515012313014e95f1afe3dcd419811b080e037b
Albin Henriksson <albhe428@student.liu.se>
2021-04-20 13:37:51 +0200

    Fix tests

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/react'
+import mockedAxios from 'axios'
 import React from 'react'
 import { act } from 'react-dom/test-utils'
 import { Provider } from 'react-redux'
@@ -7,6 +8,12 @@ import store from './store'
 
 test('renders app', async () => {
   await act(async () => {
+    const typeRes: any = {
+      data: { component_types: [], view_types: [], question_types: [], media_types: [] },
+    }
+    ;(mockedAxios.get as jest.Mock).mockImplementation(() => {
+      return Promise.resolve(typeRes)
+    })
     render(
       <Provider store={store}>
         <App />
